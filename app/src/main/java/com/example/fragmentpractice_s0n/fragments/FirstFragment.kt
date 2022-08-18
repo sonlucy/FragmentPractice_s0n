ef428a960e9168ccebe113d34947f78e64ae1c23
s0n <shson971029@naver.com>
2022-08-18 23:29:01 +0900

    프래그먼트 <-> xml 파일 결합 작업 - onCreateView 에서 담당

---
+++ app/src/main/java/com/example/fragmentpractice_s0n/fragments/FirstFragment.kt
@@ -0,0 +1,20 @@
+package com.example.fragmentpractice_s0n.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentpractice_s0n.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+}
