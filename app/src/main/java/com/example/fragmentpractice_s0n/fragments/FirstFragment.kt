89b11512405e12ef69420c081832a3799bbb61a7
s0n <shson971029@naver.com>
2022-08-18 23:34:05 +0900

    첫 프래그먼트 동작 처리 + 메인화면 xml 배치

--- app/src/main/java/com/example/fragmentpractice_s0n/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentpractice_s0n/fragments/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fragmentpractice_s0n.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -16,5 +17,18 @@ class FirstFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        changeTextBtn.setOnClickListener {
+
+//            임시로 -> 텍스트뷰 문구를 안녕하세요 로 변경
+            contentTxt.text="안녕하세요."
+
+        }
+
+    }
+
 
 }
